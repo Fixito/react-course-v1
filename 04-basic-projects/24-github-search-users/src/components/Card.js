a6496cff13e17f1a0791512f43f2e1798cc2d6c7
Fixito <thomas_3004@hotmail.fr>
2022-09-13 09:50:11 +0200

    Add Card, Followers, Info  and User components

--- 04-basic-projects/24-github-search-users/src/components/Card.js
+++ 04-basic-projects/24-github-search-users/src/components/Card.js
@@ -2,9 +2,52 @@ import React from 'react';
 import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
+
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username
+  } = githubUser;
+
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          {twitter_username && <p>@{twitter_username}</p>}
+        </div>
+        <a href={html_url}>suivre</a>
+      </header>
+      {bio && <p className='bio'>{bio}</p>}
+      <div className='links'>
+        {company && (
+          <p>
+            <MdBusiness /> {company}
+          </p>
+        )}
+        <p>
+          <MdLocationOn />
+          {location}
+        </p>
+        {blog && (
+          <a href={`https://${blog}}]`}>
+            <MdLink />
+            {blog}
+          </a>
+        )}
+      </div>
+    </Wrapper>
+  );
 };
+
 const Wrapper = styled.article`
   background: var(--clr-white);
   padding: 1.5rem 2rem;
@@ -13,7 +56,7 @@ const Wrapper = styled.article`
   border-bottom-right-radius: var(--radius);
   position: relative;
   &::before {
-    content: 'user';
+    content: 'utilisateur';
     position: absolute;
     top: 0;
     left: 0;
@@ -85,4 +128,5 @@ const Wrapper = styled.article`
     }
   }
 `;
+
 export default Card;
--- 04-basic-projects/24-github-search-users/src/components/Followers.js
+++ 04-basic-projects/24-github-search-users/src/components/Followers.js
@@ -3,7 +3,27 @@ import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 
 const Followers = () => {
-  return <h2>followers component</h2>;
+  const { followers } = React.useContext(GithubContext);
+
+  return (
+    <Wrapper>
+      <div className='followers'>
+        {followers.map((follower, index) => {
+          const { avatar_url: img, html_url, login } = follower;
+
+          return (
+            <article key={index}>
+              <img src={img} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>{html_url}</a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.article`
@@ -14,7 +34,7 @@ const Wrapper = styled.article`
   position: relative;
 
   &::before {
-    content: ' followers';
+    content: 'abonnés';
     position: absolute;
     top: 0;
     left: 0;
@@ -58,4 +78,5 @@ const Wrapper = styled.article`
     }
   }
 `;
+
 export default Followers;
