c5dde6ebb41155047a99eb51b30a86f20b685003
Thomas ROBERT <thomas_3004@hotmail.fr>
2022-12-14 14:35:43 +0100

    Setup filter products context

--- 04-projects/25-store/src/index.js
+++ 04-projects/25-store/src/index.js
@@ -12,7 +12,11 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ProductsProvider>
-    <App />
-  </ProductsProvider>
+  <React.StrictMode>
+    <ProductsProvider>
+      <FilterProvider>
+        <App />
+      </FilterProvider>
+    </ProductsProvider>
+  </React.StrictMode>
 );
