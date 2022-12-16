10957e877ead6c787b1ba0edc86b9e2be83b1090
Thomas ROBERT <thomas_3004@hotmail.fr>
2022-12-16 10:20:05 +0100

    Setup cart context and reducer

--- 04-projects/25-store/src/components/AddToCart.js
+++ 04-projects/25-store/src/components/AddToCart.js
@@ -5,7 +5,9 @@ import { FaCheck } from 'react-icons/fa';
 import { useCartContext } from '../context/cart_context';
 import AmountButtons from './AmountButtons';
 
-const AddToCart = ({ id, stock, colors }) => {
+const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
+  const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
 
@@ -55,7 +57,10 @@ const AddToCart = ({ id, stock, colors }) => {
           increase={increase}
           decrease={decrease}
         />
-        <Link to='/cart' className='btn'>
+        <Link
+          to='/cart'
+          className='btn'
+          onClick={() => addToCart(id, mainColor, amount, product)}>
           add to cart
         </Link>
       </div>
