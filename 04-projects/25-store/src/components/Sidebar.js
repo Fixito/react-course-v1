cbf5f8b2345747eaad2a6362046aca9bbb79c201
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-02-07 10:22:52 +0100

    Add cutomization with Airtable

--- 04-projects/25-store/src/components/Sidebar.js
+++ 04-projects/25-store/src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
+import Logo from './Logo.js';
 import { Link } from 'react-router-dom';
 import { useProductsContext } from '../context/products_context';
 import { FaTimes } from 'react-icons/fa';
@@ -16,7 +16,7 @@ const Sidebar = () => {
     <SidebarContainer>
       <aside className={`sidebar ${isSidebarOpen && 'show-sidebar'}`}>
         <div className='sidebar-header'>
-          <img src={logo} alt='comfy sloth' />
+          <Logo />
           <button className='close-btn' type='button' onClick={closeSidebar}>
             <FaTimes />
           </button>
